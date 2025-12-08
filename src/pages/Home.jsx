f9fb918e365b31f866ba73c054700560408e401c
Simone Massonie <simone.massonie@gmail.com>
2025-12-08 16:22:46 +0100

    implement event grid

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,15 @@
-export const Home =()=>{
-  return(
-    <div>Home</div>
-  )
-}
+import { fetchEvents } from "../api/events";
+import { useEffect, useState } from "react";
+import { EventGrid } from "../components/EventGrid";
+
+export const Home = () => {
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    fetchEvents().then((data) => setEvents(data));
+  }, []);
+  return (
+    <div>
+      <EventGrid events={events.results} />
+    </div>
+  );
+};
