8dcadbc393561b2e82624097e4ba6dbd962c09bb
Simone Massonie <simone.massonie@gmail.com>
2025-12-10 14:58:41 +0100

    add Sign In page

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_FALLBACK = "http://localhost:3001/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || API_FALLBACK;
 
 export const apiConfig = {
   baseUrl: API_BASE_URL,
